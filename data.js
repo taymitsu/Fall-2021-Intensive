60d0c26a7ada20d38d0a9b72fb1b5f462c338b6d
Tay <tmitsumori96@gmail.com>
2021-10-11 19:29:41 -0700

    added products to data.js, removed cards from html

---
+++ data.js
@@ -0,0 +1,76 @@
+const data = {[
+    "id": 1,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+}, {
+    "id": 2,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+}, {
+    "id": 3,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+}, {
+    "id": 4,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+}, {
+    "id": 5,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+}, {
+    "id": 6,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+}, {
+    "id": 7,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+}, {
+    "id": 8,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+}, {
+    "id": 9,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+
+}, {
+    "id": 10,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+}, {
+    "id": 11,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+}, {
+    "id": 12,
+    "name": "Prayer Plant",
+    "image": 
+    "desc": 
+    "price": 10.99
+]}
+
+export default data;
