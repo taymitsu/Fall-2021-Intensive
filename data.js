36bca7f2ddf331d3f3ad8cd83f950ec83763f1f7
Tay <tmitsumori96@gmail.com>
2021-10-14 11:03:52 -0700

    data for cart updated

--- data.js
+++ data.js
@@ -1,75 +1,75 @@
 const data = [{
     "id": 1,
-    "name": "Polka Dot Plant",
-    "image": "images/polka.jpeg",
-    "desc": "Happiness lies in the joy of achievement and the thrill of creative effort.",
-    "price": 25.99
+    "name": "ZZ Plant",
+    "image": "images/zz.gif",
+    "desc": "A hardy plant that can be set and forget.",
+    "price": '$23.99'
   }, {
     "id": 2,
-    "name": 'Prayer Plant',
-    "image": 'images/prayer.jpeg',
+    "name": 'Boston Fern',
+    "image": 'images/bostonfern.jpeg',
     "desc": "Tears are words that can’t be explained.",
-    "price": 5.99
+    "price": '$30.99'
   }, {
     "id": 3,
-    "name": 'Boston Fern',
-    "image": "images/bost",
-    "desc": "Anger is like a storm rising up from the bottom of your consciousness. When you feel it coming, turn your focus to your breath.",
-    "price": 5.99
+    "name": 'Prayer Plant',
+    "image": "images/prayer.jpeg",
+    "desc": "A statement piece that does nightly prayer rituals to the moon.",
+    "price": '$35.99'
   }, {
     "id": 4,
     "name": 'Money Tree',
-    "image": "images/calm_woman.gif",
+    "image": "images/moneytree.jpeg",
     "desc": "To be calm is the highest achievement of the self.",
-    "price": 5.99
+    "price": '$21.99'
   }, {
     "id": 5,
     "name": 'Pilea',
-    "image": "images/curious_owl.gif",
+    "image": "images/pilea.gif",
     "desc": "I have no special talents. I am only passionately curious.",
-    "price": 5.99
+    "price": '$22.99'
   }, {
     "id": 6,
-    "name": 'ZZ Plant',
-    "image": "images/disgust_theoffice.gif",
+    "name": 'Polka Dot Plant',
+    "image": "images/polka.jpeg",
     "desc": "Not all disgust need produce symptoms of nausea, nor all nausea mark the presence of disgust.",
-    "price": 5.99
+    "price": '$18.99'
   }, {
     "id": 7,
-    "name": 'Hoya Kerri',
-    "image": "images/energetic_puppy.gif",
+    "name": 'Sansaveria',
+    "image": "images/whitesnake.jpeg",
     "desc": "Be the energy you want to attract.",
-    "price": 5.99
+    "price": '$25.99'
   }, {
     "id": 8,
-    "name": 'Sansaveria',
-    "image": "images/gloomy_girl.gif",
+    "name": 'Monstera Deliciosa',
+    "image": "images/monstera.jpeg",
     "desc": "All was black, gloomy and awful. There was no light at the end of the tunnel - or if there was, it was an oncoming train.",
-    "price": 5.99
+    "price": '$25.99'
   }, {
     "id": 9,
     "name": 'Philodendron Brasil',
-    "image": "images/scared_spongebob.gif",
+    "image": "images/pphilo.jpeg",
     "desc": "The only thing we have to fear is fear itself.",
-    "price": 5.99
+    "price": '$20.99'
   }, {
     "id": 10,
-    "name": 'Monstera Deliciosa',
-    "image": "images/shy_dog.gif",
+    "name": 'Hoya Kerrii',
+    "image": "images/hoya.gif",
     "desc": "When a shy person smiles, it's like the sun coming out.",
-    "price": 5.99
+    "price": '$14.99'
   }, {
     "id": 11,
     "name": 'Alocasia',
-    "image": "images/sleepy_cat.gif",
+    "image": "images/alocasia.jpeg",
     "desc": "Your future depends on your dreams, so go to sleep.",
-    "price": 5.99
+    "price": '$35.99'
   }, {
     "id": 12,
     "name": 'Marble Queen Pothos',
-    "image": "images/surprised_2.gif",
+    "image": "images/pothos.jpeg",
     "desc": "The only thing that should surprise us is that there are still some things that can surprise us.",
-    "price": 18.00
+    "price": '$18.99'
   }]
   
   export default data;
